0c0a58d5b267d7cfa3b9b51973821fc6de9cbdaf
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-05-15 22:19:08 +0700

    Lab_05

--- OtherProject/src/hust/soict/hedspi/garbage/GarbageCreator.java
+++ OtherProject/src/hust/soict/hedspi/garbage/GarbageCreator.java
@@ -1,5 +1,24 @@
 package hust.soict.hedspi.garbage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class GarbageCreator {
 
+    public static void main(String[] args) {
+        try {
+            File inputFile = new File("src/Files/test.txt");
+            long start = System.currentTimeMillis();
+            Scanner sc = new Scanner(inputFile);
+            String s = "";
+            while(sc.hasNextLine()) {
+                s += sc.nextLine() + "\n";
+            }
+            System.out.printf("Processing time: %d", System.currentTimeMillis() - start);
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
